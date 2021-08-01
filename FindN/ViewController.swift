3a42f8fa471d53fb268d6b190f3e0a1eab453fba
Nikita <tshcheslav@gmail.com>
2021-08-01 15:46:05 +0300

    Initial Commit

---
+++ FindN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindN
+//
+//  Created by 1 on 01.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
